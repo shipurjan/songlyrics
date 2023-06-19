a795db4d8e0f36a46f467451ef070fbf414d32fa
Cyprian Zdebski <cyprian.zdebski@protonmail.com>
2023-06-19 21:51:47 +0200

    changed sumthings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Layout } from "./layout";
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home/Home';
 import { SongList } from './pages/songlist/SongList';
 import { Joke } from './pages/joke/Joke';
@@ -9,15 +9,13 @@ import { Dictionary } from './pages/dictionary/Dictionary';
 export default function App() {
   return (
     <Layout>
-      <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/songlist" element={<SongList />} />
           <Route path="/joke" element={<Joke />} />
           <Route path="/dictionary" element={<Dictionary />} />
         </Routes>
-      </Router>
     </Layout>
   );
 };
