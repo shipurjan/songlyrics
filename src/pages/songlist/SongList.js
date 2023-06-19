9bf2ed8a8d4d6944e9d7e58ba7e03b1704b5a144
Cyprian Zdebski <cyprian.zdebski@protonmail.com>
2023-06-19 19:52:16 +0200

    Example joke request

--- src/pages/songlist/SongList.js
+++ src/pages/songlist/SongList.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import "./SongList.css";
+import Axios from 'axios';
 
 export const SongList = () => {
+  const [joke, setJoke] = useState("")
+  const getJoke = () => {
+    Axios.get("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => {
+        console.log(response);
+        setJoke(<>{response.data.setup}<br/>{response.data.punchline}</>)
+    })
+  }
+
   return (<>
-      SongList
+      SongList<br/>
+      <button onClick={getJoke}>Get joke</button>
+      <div>
+      {joke}
+      </div>
       </>
   );
 }
